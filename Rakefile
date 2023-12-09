c670460bd5de9d09252235c2e01992637bde6ebb
Dru Ibarra <Druwerd@gmail.com>
2023-12-09 13:51:48 -0800

    adds yard docs for controllers and models

--- Rakefile
+++ Rakefile
@@ -8,7 +8,7 @@ Rails.application.load_tasks
 require 'yard'
 
 YARD::Rake::YardocTask.new do |t|
- t.files   = ['lib/**/*.rb']
+ t.files   = ['lib/**/*.rb', 'app/controllers/**/*.rb', 'app/models/**/*.rb']
  t.options = ['--any', '--extra', '--opts']
  t.stats_options = ['--list-undoc']
 end
--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,9 +1,21 @@
+# frozen_string_literal: true
+
+# Controller for handling weather-related actions.
 class WeatherController < ApplicationController
+  # Clears alert and notice messages before certain actions.
   before_action :clear_notices, :set_form_fields
 
+  # Renders the index view.
   def index
   end
 
+  # Retrieves and displays current weather based on user-input parameters.
+  #
+  # If valid parameters are provided, it sets instance variables for the view,
+  # caches the weather data, and renders the current view. Otherwise, it alerts
+  # the user about missing or invalid parameters and renders the index view.
+  #
+  # @raise [StandardError] Reraised to render Rails error page.
   def current
     if valid_params?
       @zip_code, _ = weather_params[:zip_code].split("-")
@@ -25,6 +37,7 @@ class WeatherController < ApplicationController
 
   private
 
+  # Sets instance variables for form fields based on user-input parameters.
   def set_form_fields
     @street = weather_params[:street]
     @city = weather_params[:city]
@@ -32,19 +45,29 @@ class WeatherController < ApplicationController
     @zip_code = weather_params[:zip_code]
   end
 
+  # Checks if the provided parameters are valid.
+  #
+  # Parameters are considered valid if all required parameters are present,
+  # and the zip code follows the correct format.
+  #
+  # @return [Boolean] True if parameters are valid, false otherwise.
   def valid_params?
     required_params.all? { |p| weather_params[p].present? } &&
       valid_zip?
   end
 
+  # Checks if the provided zip code is in the correct format.
+  #
+  # @return [Boolean] True if the zip code is in the correct format, false otherwise.
   def valid_zip?
     weather_params[:zip_code].to_s.match?(/^\d{5}(\-\d{4})?$/)
   end
 
+  # Alerts the user about missing or invalid parameters.
   def alert_invalid_params
-    missing_params = required_params.select{ |p| weather_params[p].blank? }
+    missing_params = required_params.select { |p| weather_params[p].blank? }
     missing_param_names = missing_params
-      .map{ |p| p.to_s.humanize }.join(", ")
+      .map { |p| p.to_s.humanize }.join(", ")
 
     message = ""
     message += "Missing #{missing_param_names}. " if missing_params.any?
@@ -53,19 +76,29 @@ class WeatherController < ApplicationController
     flash.alert = message.strip
   end
 
+  # Clears alert and notice messages.
   def clear_notices
     flash.alert = nil
     flash.notice = nil
   end
 
+  # Retrieves the OpenWeatherConnector instance.
+  #
+  # @return [Services::OpenWeatherConnector] The OpenWeatherConnector instance.
   def weather_connector
     @weather_connect ||= Services::OpenWeatherConnector.new
   end
 
+  # Retrieves the permitted weather parameters.
+  #
+  # @return [ActionController::Parameters] Permitted weather parameters.
   def weather_params
     params.permit(*required_params)
   end
 
+  # Defines the required parameters for weather-related actions.
+  #
+  # @return [Array<Symbol>] Array of symbols representing required parameters.
   def required_params
     [:street, :city, :state, :zip_code]
   end

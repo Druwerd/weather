417316d03dda50766d065751e752dd0be51cdf56
Dru Ibarra <Druwerd@gmail.com>
2023-12-09 09:18:55 -0800

    - refactoring - set form field values

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,12 +1,12 @@
 class WeatherController < ApplicationController
-  before_action :clear_notices
+  before_action :clear_notices, :set_form_fields
 
   def index
   end
 
   def current
     if valid_params?
-      @zip_code = params[:zip_code]
+      @zip_code = weather_params[:zip_code]
 
       flash.notice = "Cached"
       @weather = Rails.cache.fetch(@zip_code.to_s, expires_in: 30.minutes) do
@@ -21,12 +21,22 @@ class WeatherController < ApplicationController
 
   private
 
+  def set_form_fields
+    @street = weather_params[:street]
+    @city = weather_params[:city]
+    @state = weather_params[:state]
+    @zip_code = weather_params[:zip_code]
+  end
+
   def valid_params?
-    required_params.all? { |p| params[p].present? }
+    required_params.all? { |p| weather_params[p].present? }
   end
 
   def alert_missing_params
-    missing_param_names = required_params.map{|p| p.to_s.humanize }.join(",")
+    missing_params = required_params.select{ |p| weather_params[p].blank? }
+    missing_param_names = missing_params
+      .map{ |p| p.to_s.humanize }.join(", ")
+
     flash.alert = "Missing #{missing_param_names}"
   end
 
@@ -40,10 +50,10 @@ class WeatherController < ApplicationController
   end
 
   def weather_params
-    params.require(:weather).permit(:street, :city, :state, :zip_code)
+    params.permit(*required_params)
   end
 
   def required_params
-    [:zip_code]
+    [:street, :city, :state, :zip_code]
   end
 end
